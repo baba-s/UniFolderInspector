0c224db0ba6b89543d2ad8cc26e9aab9471559c9
baba_s <shotababa1206@gmail.com>
2020-08-20 17:26:06 +0900

    first commit

---
+++ Editor/FolderInspector.cs
@@ -0,0 +1,31 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Kogane.Internal
+{
+	[CustomEditor( typeof( DefaultAsset ) )]
+	internal sealed class FolderInspector : Editor
+	{
+		public override void OnInspectorGUI()
+		{
+			var path = AssetDatabase.GetAssetPath( target );
+
+			if ( !AssetDatabase.IsValidFolder( path ) ) return;
+			if ( !path.StartsWith( "Assets/" ) ) return;
+
+			GUI.enabled = true;
+
+			if ( GUILayout.Button( "Create Folder" ) )
+			{
+				EditorApplication.ExecuteMenuItem( "Assets/Create/Folder" );
+			}
+
+			if ( GUILayout.Button( "Create C# Script" ) )
+			{
+				EditorApplication.ExecuteMenuItem( "Assets/Create/C# Script" );
+			}
+
+			GUI.enabled = false;
+		}
+	}
+}
